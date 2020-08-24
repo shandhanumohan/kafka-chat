51230eb99e5087c810c5baa51626fd31e8ad5b77
shandhanu05@gmail.com <Git4sh@n>
2020-08-24 14:40:38 +0530

    adding ui

---
+++ vui/src/services/chatapi.js
@@ -0,0 +1,24 @@
+import Axios from "axios";
+
+const api = Axios.create({
+    baseURL: '/api/',
+});
+
+const chatAPI = {
+    getMessages: (groupId) => {
+        console.log('Calling get messages from API');
+        return api.get(`messages/${groupId}`);
+    },
+
+    sendMessage: (username, text) => {
+        let msg = {
+            sender: username,
+            content: text
+        }
+        console.log('message' + JSON.stringify(msg));
+        return api.post(`send`, msg);
+    }
+}
+
+
+export default chatAPI;
